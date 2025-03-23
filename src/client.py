7e2048f2725e23998cf44c75802f884b438f14c3
rhoninn11 <rhoninn11@gmail.com>
2025-03-23 12:04:28 +0100

    - using subrepo for proto defs

--- src/client.py
+++ src/client.py
@@ -8,14 +8,21 @@ import proto.comfy_pb2_grpc as pb2_grpc
 
 from skimage import io
 
-from utils_mea import *
+from utils_proto import *
 from utils import proj_asset, file2json2obj, ensure_path_exist, Timeline
 
+def clone(mesg: str):
+    print(mesg)
+    exit(1)
+
+
+# channer leduction fns 
 def uno_channel(img: np.ndarray) -> np.ndarray:
     return img[:,:, 0:1]
 
 def trio_channel(img: np.ndarray) -> np.ndarray:
     return img[:,:, 0:3]
+# ------------------------
 
 def load_prompt():
     import json
@@ -33,6 +40,7 @@ def save_img(name: str):
     name.split()
 
 
+# for safe rpc connection over tcp
 def _load_credential_from_file(filepath):
     real_path = os.path.abspath(filepath)
     with open(real_path, "rb") as f:
@@ -42,19 +50,30 @@ def _load_credential_from_file(filepath):
 SERVER_CERTIFICATE = _load_credential_from_file("assets/credentials/localhost.crt")
 SERVER_CERTIFICATE_KEY = _load_credential_from_file("assets/credentials/localhost.key")
 ROOT_CERTIFICATE = _load_credential_from_file("assets/credentials/root.crt")
+# -------------------------------------------------------------------------------------
 
 
-def sequence_gen(opt: pb2.Options, stub: pb2_grpc.ComfyStub):
+
+RPC_STUB: pb2_grpc.ComfyStub = None
+def assert_stub_exist():
+    if RPC_STUB != None:
+        return
+    
+    clone("!!! stub not initialized")
+
+
+# comfy grpc examples
+def sequence_gen(opt: pb2.Options):
     save_dir = "fs/seq"
     ensure_path_exist(save_dir)
 
     for i, _power in enumerate(np.linspace(0,0.25,5).tolist()):
         opt.img_power = _power
-        stub.SetOptions(opt)
-        _result = stub.Inpaint(pb2.Empty())
+        RPC_STUB.SetOptions(opt)
+        _result = RPC_STUB.Inpaint(pb2.Empty())
         io.imsave(f'{save_dir}/out_img_{i:02}.png', img_proto_2_np(_result))
 
-def sequence_adapter_run(opt: pb2.Options, stub: pb2_grpc.ComfyStub, proto_img: pb2.Image):
+def sequence_adapter_run(opt: pb2.Options, proto_img: pb2.Image):
     save_dir = "fs/seq_ada"   
     ensure_path_exist(save_dir)
     print("+++ Elo", proto_img.info)
@@ -71,8 +90,8 @@ def sequence_adapter_run(opt: pb2.Options, stub: pb2_grpc.ComfyStub, proto_img: 
             # print(x_off, y_off, type(x_off), type(win_size), type(img_size))
             min_img = np_ref_img[y_off:y_off + win_size, x_off:x_off + win_size, :]
 
-            stub.SetCrop(img_np_2_proto(min_img))
-            result = stub.Ipnet(pb2.Empty())
+            RPC_STUB.SetCrop(img_np_2_proto(min_img))
+            result = RPC_STUB.Ipnet(pb2.Empty())
             np_result = img_proto_2_np(result).copy()
 
             min_img = min_img[::2, ::2, :]
@@ -80,12 +99,12 @@ def sequence_adapter_run(opt: pb2.Options, stub: pb2_grpc.ComfyStub, proto_img: 
             io.imsave(f'{save_dir}/img_{x_off}_{y_off}.png', np_result)
  
 
-def single_adapter_run(opt: pb2.Options, stub: pb2_grpc.ComfyStub):
+def single_adapter_run(opt: pb2.Options, ):
     save_dir = "fs"
     opt.img_power = 0.0
     opt.inpt_flag = pb2.SDXL
-    stub.SetOptions(opt)
-    _result = stub.Ipnet(pb2.Empty())
+    RPC_STUB.SetOptions(opt)
+    _result = RPC_STUB.Ipnet(pb2.Empty())
     print("first step")
     io.imsave(f'{save_dir}/out_img_sgl.png', img_proto_2_np(_result))   
 
@@ -105,6 +124,7 @@ def single_gen(opt: pb2.Options, stub: pb2_grpc.ComfyStub):
     _result = stub.Inpaint(pb2.Empty())
     print("second step")
     io.imsave(f'{save_dir}/out_img_sgl.png', img_proto_2_np(_result))
+# -----------------------------------------------------------------------
 
 
 def serdes_pipe(img_proto: pb2.Image, write=False) -> pb2.Image:
@@ -116,6 +136,7 @@ def serdes_pipe(img_proto: pb2.Image, write=False) -> pb2.Image:
     return pb2.Image.FromString(bin_data)
 
 def start_client():
+    global RPC_STUB
     port = 50051
 
     # spawn assets in fs
@@ -129,7 +150,7 @@ def start_client():
     channel_options = [('grpc.ssl_target_name_override', 'localhost')] # tmp workaround
     
     channel = grpc.secure_channel(endpoint, credentials, options=channel_options)
-    stub = pb2_grpc.ComfyStub(channel)
+    RPC_STUB= pb2_grpc.ComfyStub(channel)
 
     img_np = trio_channel(load_image('img.png'))
     mask_np = uno_channel(load_image('mask.png'))
@@ -146,16 +167,18 @@ def start_client():
     img_proto = serdes_pipe(img_proto, True)
 
     mask_proto = img_np_2_proto(mask_np)
-
+    
+    assert_stub_exist()
     with Timeline() as time_messure:
-        stub.SetImage(img_proto)
-        stub.SetMask(mask_proto)
-        stub.SetOptions(opts)
+        RPC_STUB.SetImage(img_proto)
+        RPC_STUB.SetMask(mask_proto)
+        RPC_STUB.SetOptions(opts)
         
-        # sequence_gen(_opt, stub)
-        # single_gen(_opt, stub)
-        # single_adapter_run(_opt, stub)
-        sequence_adapter_run(opts, stub, img_proto)
+        # test playground
+        # sequence_gen(_opt)
+        # single_gen(_opt)
+        # single_adapter_run(_opt)
+        sequence_adapter_run(opts, img_proto)
         print("do nothing")
     
 
---
+++ src/utils_proto.py
@@ -0,0 +1,18 @@
+
+import numpy as np
+import proto.comfy_pb2 as pb2
+
+def img_proto_2_np(img_proto: pb2.Image) -> np.ndarray:
+    img_np = np.frombuffer(img_proto.pixels, dtype=np.uint8)
+    img_np = img_np.reshape(img_proto.info.height, img_proto.info.width, img_proto.info.img_type)
+    return img_np
+
+
+def img_np_2_proto(img_np: np.ndarray) -> pb2.Image:
+    print(img_np.shape)
+    width, height, ch = img_np.shape
+    img_type = pb2.ImgType.RGB if ch == 3 else pb2.ImgType.MONO
+    img_info = pb2.ImgInfo(width=width, height=height, img_type=img_type)
+    byte_data = img_np.tobytes()
+    img_proto = pb2.Image(info=img_info, pixels=byte_data)
+    return img_proto
